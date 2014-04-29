fb48f892f904f2c5f4cc545994b06883344ab97d
OLC-Bioinformatics <adam.koziol@inspection.gc.ca>
2014-04-29 13:24:05 -0400

    Commit

--- rawReadSipping.pl
+++ rawReadSipping.pl
@@ -1,7 +1,7 @@
 #usr/bin/perl
 
 use warnings;
-use strict;
+#use strict;
 use Cwd;
 use Time::Piece;
 use threads;
@@ -11,15 +11,12 @@ use Bio::SeqIO;
 use File::Copy;
 use Data::Dumper qw(Dumper);
 
-my $path = getcwd;
-
 # This start time will be used in calculating the total time of the run
 my $start_time = time;
 
 # Initialize variables
-my ($sequenceName, $forwardReverse, $geneName, @folders, $fastaTitle, $reads, $investigator, $flowCell);
-my (%results, %sampleSheet);
-my @lengths = ("50", "45", "40", "35", "30", "25", "20");
+my ($sequenceName, $forwardReverse, $geneName, @folders, $fastaTitle, $reads, $investigator, $flowCell, $project);
+my (%results, %sampleSheet, %sippr, %targets);
 
 my $lane = 1;
 
@@ -45,15 +42,21 @@ $flowCell = $1;
 
 my $folderPath = "/media/nas/akoziol/WGS/RawReadSipping/$folder";
 
-make_path($folderPath);
+
+# Make the appropriate folder in the required location - I'm not sure whether the permissions section at the end is necessary - it worked when I was trying to make it work, so, for now, it stays
+make_path($folderPath, {owner => "blais", group => "blais", mode => 0777});
+
+open(OUTPUT, ">", "$folderPath/SampleSheet_modified.csv") or die $!;
+#system("chown blais:blais $folderPath/SampleSheet_modified.csv");
+#system ("chmod 777 $folderPath/SampleSheet_modified.csv");
+print OUTPUT "FCID,Lane,SampleID,SampleRef,Index,Description,Control,Recipe,Operator,SampleProject\n";
 
 chdir($miSeqPath . "/" . $folder);
-open(OUTPUT, ">", "$folderPath/SampleSheet_modified.csv") or die $!;
-print OUTPUT "FCID\tLane\tSampleID\tSampleRef\tIndex\tDescription\tControl\tRecipe\tOperator\tSampleProject\n";
+
 # Open the parameters file to determine the run length - this is necessary, as we will be pulling data once the first index has been processed (just over halfway through the run)
 open(SHEET, "<", "SampleSheet.csv");
 while (<SHEET>) {
-	# Grab the investigator name
+	# Grab the investigator name - perhaps because the input file is a CSV, the chop function (x2) seems to be required to format the cells properly
 	if (/Investigator Name/) {
 		($investigator = $_) =~ s/Investigator Name,//;
 		chop $investigator;
@@ -81,52 +84,93 @@ while (<SHEET>) {
 			#print "$_";
 			my @line = split(/,/);
 			# $line[5], $line[7]: index 1 and 2, respectively. $line[9]: description
-			chop $line[5]; chop $line[7]; chop $line[9]; chop $line[9];
-			my $index = $line[5] . "-" . $line[7];
-			print OUTPUT "$flowCell\t", 	# flowcell
-						 "$lane\t",			# lane 1
-						 "$line[0]\t",  	# sample ID
-						 "no_ref\t",    	# no_ref sampleRef
-						 "$index\t",    	# index
-						 "$line[9]\t",  	# description
-						 "N\t",      		# N control
-						 "NA\t",		 	# NA recipe
-						 "$investigator\t", # operator
-						 "$line[8]\n";		# sample_project
-
+			#chop $line[5]; chop $line[7];
+			chop $line[9]; chop $line[9];
+			$project = $line[8];
+			my $index = $line[5];
+			#my $index = $line[5] . "-" . $line[7];
+			print OUTPUT "$flowCell,", 	# flowcell
+						 "$lane,",			# lane 1
+						 "$line[0],",  	# sample ID
+						 "no_ref,",    	# no_ref sampleRef
+						 "$index,",    	# index
+						 "$line[9],",  	# description
+						 "N,",      		# N control
+						 "NA,",		 	# NA recipe
+						 "$investigator,", # operator
+						 "$project\n";		# sample_project
 		}
 	}
 }
 
-=com
-# The files must be in the "query" subfolder. This subfolder must only have sequences that you wish to examine, or the program won't be able to find them
-chdir ("$path/query");
+close SHEET;
+close OUTPUT;
 
+# Find out how many cycles have been completed
+chdir($miSeqPath . "/" . $folder . "/Thumbnail_images/L001");
 
-#Grab the fastq files for manipulations
-my @sequenceFiles = glob("*.fastq");
+my @cycleNum = glob("*C*");
+my $cycles = scalar @cycleNum;
 
-###Note, this piece of code will only have to be used when generating truncated reads
-# Use fastq_trimmer to trim the fastq files to the appropriate lengths
-foreach my $sequenceFile (@sequenceFiles) {
-	# Determine the forward and reverse fastq files based on name (eg. OLC797_R1_001.fastq vs OLC797_R2_001.fastq)
-	if ($sequenceFile =~ /R1/) {
-		$forwardReverse = "F";
-	} elsif ($sequenceFile =~ /R2/) {
-		$forwardReverse = "R";
-	}
-	($sequenceName = $sequenceFile) =~ s/_R\d.+//;
-	foreach my $length (@lengths) {
-		make_path("$path/query/$length/$sequenceName");
-		unless (-e("$length/$sequenceName/$sequenceName" . "$forwardReverse" . "_$length.fastq")) {
-			print "Trimming reads from $sequenceName direction $forwardReverse to $length nucleotides in length\n";
-			system ("fastx_trimmer -Q33 -l $length -i $sequenceFile -o $length/$sequenceName/$sequenceName" . "$forwardReverse" . "_$length.fastq");
+# As the number of cycles required is the number of forward reads + the index(8)
+my $readsNeeded = $reads + 8;
+
+# A while loop that waits until the required number of cycles has been achieved
+while ($cycles < $readsNeeded) {
+	running_time("Currently at cycle $cycles. Need to wait until the MiSeq reaches cycle $readsNeeded.");
+	sleep(300);
+	chdir($miSeqPath . "/" . $folder . "/Data/Intensities/BaseCalls/L001");
+	@cycleNum = glob("*C*");
+	$cycles = scalar @cycleNum;
+}
+
+# Adding 0 to $reads converts it from a string to a number
+my $numReads = $reads + 0;
+
+# Sets the base-mask string, which is important for determining which bases to use from each run
+my $baseMask = "Y" . $numReads . ",I8,n*,n*";
+
+# Call configureBclToFastq.pl - look into preventing the compression of the .fastq files
+unless(-e("$folderPath/Unaligned")) {
+	system("configureBclToFastq.pl --input-dir $miSeqPath/$folder/Data/Intensities/BaseCalls/ --output-dir $folderPath/Unaligned --force --sample-sheet $folderPath/SampleSheet_modified.csv --mismatches 1 --no-eamss --fastq-cluster-count 0 --use-bases-mask $baseMask");
+	chdir("$folderPath/Unaligned");
+	system("nohup make -j 16 r1");
+}
+
+# Chdir to the working directory
+chdir("$folderPath/Unaligned/Project_$project") or die $!;
+my $path = getcwd;
+
+# This uncompresses the fastq files - I'm looking at editing the configureBclToFastq.pl script or one of its modules to try and avoid compression in the first place
+opendir(DIR, $path) or die "can't find $path: $!";
+while (defined(my $file = readdir(DIR))) {
+	# Ignore special files
+	next if $file =~ /^\.\.?$/;
+	# Ignore folders without sequence data
+	if ($file =~ m/Sample_/) {
+		if (-d $path ."/". $file){
+			chdir $path ."/". $file;
+			my @files = glob("*.gz");
+			(my $folder = $file) =~ s/Sample_//g;
+			(my $unzip = $files[0]) =~ s/.gz//g;
+			(my $filename = $files[0]) =~ s/_L001|.gz//g;
+			running_time("Now extracting $filename");
+			make_path("$path/$folder");
+			make_path("$path/query");
+			move($files[0],"$path/$folder/$files[0]");
+			system("gzip -d $path/$folder/$files[0]");
+			chdir("$path/$folder");
+			if (-f $unzip){unlink "$path/$folder/$files[0]"};
+			move("$path/$folder/$unzip","$path/$folder/$filename");
+			copy("$path/$folder/$filename", "$path/query/$filename");
+			if (-f $filename){rmdir("$path/$file") or die $!};
+			chdir("$path");
 		}
 	}
 }
 
-
-chdir ("$path/target");
+my $filesPath = "/media/nas/akoziol/WGS/RawReadSipping/target";
+chdir ("$filesPath");
 my @targetGenes = glob("*.fa");
 
 foreach my $gene (@targetGenes) {
@@ -138,80 +182,67 @@ foreach my $gene (@targetGenes) {
 	unless (-e ("$geneName.fai")) {
 		system ("samtools faidx $gene");
 	}
+	open(TARGETS, "<", "$gene");
+	while (<TARGETS>) {
+		if (/^\>/) {
+			(my $targetTitle = $_) =~ s/>//;
+			chomp $targetTitle;
+			$targets{$geneName}{$targetTitle} = "+";
+		}
+	}
 }
-##### smalt index -k 5 -s 1 eae eae.fa
+# The files must be in the "query" subfolder. This subfolder must only have sequences that you wish to examine, or the program won't be able to find them
+chdir ("$path/query");
 
-foreach my $length (@lengths) {
-	chdir ("$path/query/$length");
-	opendir(DIR, "$path/query/$length") or die "can't find $path/query/$length: $!";
-	while (defined(my $folder = readdir(DIR))) {
-		# Ignore special files
-		next if $folder =~ /^\.\.?$/;
-		if (-d "$path/query/$length/$folder"){
-			#print "$length/$folder\n";
-			chdir ("$path/query/$length/$folder");
-			my @fastqF = glob("*F_$length.fastq");
-			my @fastqR = glob("*R_$length.fastq");
-			foreach my $gene (@targetGenes) {
-				chdir ("$path/query/$length/$folder");
-				(my $geneName = $gene) =~ s/.fa//;
-				make_path("$path/results/$geneName/$folder/$length");
-				my $name = "$folder" . "_F_$geneName";
-				# Perform smalt mapping
-				unless (-e ("$path/results/$geneName/$folder/$length/$name.sam")) {
-					system("smalt map -f samsoft -o $path/results/$geneName/$folder/$length/$name.sam -n 24 $path/target/$geneName @fastqF");
-				}
-				chdir ("$path/results/$geneName/$folder/$length");
-				# Use samtools view to convert sam to bam
-				unless (-e("$name.bam")){
-					system ("samtools view -b -S $name.sam -o $name.bam");
-				}
-				# Sort the bam file
-				unless (-e("$name" . "_sorted.bam")) {
-					system ("samtools sort $name.bam $name" . "_sorted");
-				}
-				# Produce a fastq file from the sorted bam file using the reference and these piped tools
-				unless (-e("$name" . "_consensus.fastq")) {
-					system ("samtools mpileup -uf $path/target/$gene $name" . "_sorted.bam | bcftools view -cg - | vcfutils.pl vcf2fq > $name" . "_consensus.fastq");
-				}
-				# Use BioPerl to convert the fastq to fasta
-				unless (-e("$name" . "_consensus.fa")) {
-					my $seqin = Bio::SeqIO -> new (-format => 'fastq',-file => "$name" . "_consensus.fastq");
-					my $seqout = Bio::SeqIO -> new (-format => 'fasta',-file => ">$name" . "_consensus.fa");
-					while (my $seq_obj = $seqin -> next_seq) {
-	   					$seqout -> write_seq($seq_obj);
-					}
-				}
-				make_path("$path/results/reports");
-				copy("$name" . "_consensus.fa", "$path/results/reports/$name" . "_$length" . "_consensus.fa");
-				open(FASTA, "<", "$name" . "_consensus.fa");
-				while (<FASTA>) {
-					if (/^\>/) {
-						($fastaTitle = $_) =~ s/>//;
-						chomp $fastaTitle;
-						# %results -> strain name; length of reads; target gene series name; target gene name; presence
-						$results{$folder}{$length}{$geneName}{$fastaTitle} = "+"
-					}
+#Grab the fastq files for manipulations
+my @fastq = glob("*.fastq");
+
+# Use a multithreading approach
+while (scalar(@threads) < @cpus) {
+		# Loop through each file to be subtyped
+		foreach my $file (@fastq) {
+				foreach my $gene (@targetGenes) {
+				# Make a new thread using the blast subroutine - pass appropriate values to subroutine
+				my $r = threads->new(\&rawMapping, $path, $filesPath, $file, $gene, \%targets);
+				# Output data to @threads. Right now, all this does is increase the size for the scalar(@threads) portion of the while loop
+				push(@threads,$r);
+			}
+		}
+	}
+	#my $time =  localtime;
+	#my $date = $time->ymd;
+	#my $hms = $time->hms("_");
+	#$dateTime = $date.$hms;
+
+	# This loop ensures that each thread is complete before terminating
+	foreach (@threads) {
+		my $num = $_->join;
+		# As the values returned from the blast subroutine are a hash of hashes, $num must be treated as such - %$num
+		# $results{$file}{$geneName}{$fastaTitle} = "+"
+		foreach my $geneTarget (sort keys %$num) {
+			foreach my $strain (sort keys %{ $$num{$geneTarget} }) {
+				foreach my $fastaTitle (sort keys %{ $$num{$geneTarget}{$strain} }){
+					chomp $fastaTitle;
+					$sippr{$geneTarget}{$strain}{$fastaTitle} = $$num{$geneTarget}{$strain}{$fastaTitle}
 				}
 			}
 		}
 	}
-}
-
-#print Dumper(%results);
-
-foreach my $strain (sort keys %results) {
-	print "$strain\n";
-	foreach my $nt (sort keys %{ $results{$strain} }) {
-		print "$nt\t";
-		foreach my $geneTarget (sort keys %{ $results{$strain}{$nt} }){
-			foreach my $marker (sort keys %{ $results{$strain}{$nt}{$geneTarget} }) {
-				print "$marker\t";
+	chdir ("$path");
+	foreach my $a (sort keys %sippr) {
+		foreach my $b (sort keys %{$sippr{$a}}) {
+			foreach my $c (sort keys %{$sippr{$a}{$b}}) {
+				print "$a\t$b\t$c\t$sippr{$a}{$b}{$c}\n";
 			}
 		}
-		print "\n";
 	}
-}
+
+#print Dumper(%sippr);
+
+#print Dumper(%targets);
+
+#make_path("$path/reports");
+
 # Return the run time
 my $end_time = time;
 my $total_time = $end_time - $start_time;
@@ -222,8 +253,81 @@ print "The total run time was $legible_time seconds.\n\n";
 
 exit;
 
+###############################################################################
+sub rawMapping {
+	my ($path, $filesPath, $rawFile, $gene, $targets) = @_;
+	(my $geneName = $gene) =~ s/.fa//;
+	(my $file = $rawFile) =~ s/_R1_001.fastq//;
+	my %targetGenes = %$targets;
+	make_path("$path/results/tmp");
+	chdir ("$path/query");
+	my $name = "$file" . "_$geneName";
+	# Perform smalt mapping
+	unless (-e ("$path/results/tmp/$name.sam")) {
+		system("smalt map -f samsoft -o $path/results/tmp/$name.sam -n 24 $filesPath/$geneName $rawFile");
+	}
+	chdir ("$path/results/tmp");
+	# Use samtools view to convert sam to bam
+	unless (-e("$name.bam")){
+		system ("samtools view -b -S $name.sam -o $name.bam");
+	}
+	# Sort the bam file
+	unless (-e("$name" . "_sorted.bam")) {
+		system ("samtools sort $name.bam $name" . "_sorted");
+	}
+	# Produce a fastq file from the sorted bam file using the reference and these piped tools
+	unless (-e("$name" . "_consensus.fastq")) {
+		system ("samtools mpileup -uf $filesPath/$gene $name" . "_sorted.bam | bcftools view -cg - | vcfutils.pl vcf2fq > $name" . "_consensus.fastq");
+	}
+	# Use BioPerl to convert the fastq to fasta
+	unless (-e("$name" . "_consensus.fa")) {
+		my $seqin = Bio::SeqIO -> new (-format => 'fastq',-file => "$name" . "_consensus.fastq");
+		my $seqout = Bio::SeqIO -> new (-format => 'fasta',-file => ">$name" . "_consensus.fa");
+		while (my $seq_obj = $seqin -> next_seq) {
+			$seqout -> write_seq($seq_obj);
+		}
+	}
 
-exit;
+	copy("$name" . "_consensus.fa", "$path/results/$name" . "_consensus.fa");
+	open(FASTA, "<", "$name" . "_consensus.fa");
+	while (<FASTA>) {
+		if (/^\>/) {
+			($fastaTitle = $_) =~ s/>//;
+			chomp $fastaTitle;
+			$results{$geneName}{$file}{$fastaTitle} = "+";
+		}
+	}
+
+
+	foreach my $gName (sort keys %targetGenes) {
+		foreach my $gTitle (sort keys %{ $targetGenes{$gName} }) {
+			if (exists $results{$gName} and exists $results{$gName}{$file}{$gTitle}) {
+				# %results -> strain name; length of reads; target gene series name; target gene name; presence
+				$resultsOut{$gName}{$file}{$gTitle} = "+";
+				#print "$gName $file $gTitle Found key!\n";
+			} elsif (not exists $results{$gName}) {
+				next;
+
+			} else {
+				$resultsOut{$gName}{$file}{$gTitle} = "-";
+				#print "$gName $file $gTitle Key absent!\n";
+			}
+		}
+	}
+
+
+#print Dumper(%resultsOut);
+	#chdir ("$path");
+	return \%resultsOut;
+}
+
+##########################################################
+# This subroutine allows the printing of the local time each time it is invoked - it allows for the user to see how much time has passed
+sub running_time {
+	my $time =  localtime;
+	my $hms = "[" . $time->hms . "] @_\n";
+	print $hms;
+}
 
 #################################################################
 sub commas {
@@ -232,6 +336,4 @@ sub commas {
 	(@_ == 1) ? $_[0]		:
 	(@_ == 2) ? join(", ", @_)	:
 		    join("$sepchar ", @_[0 .. ($#_-1)], "$_[-1]");
-
-
 }
